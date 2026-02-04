b3a90ca66f73c5709ec346777dc60c890daa40d0
anoopkarnik <danoopkarnik@gmail.com>
2026-02-04 19:08:18 +0530

    Home Page corrected

--- apps/web/blocks/home/SessionSettings.tsx
+++ apps/web/blocks/home/SessionSettings.tsx
@@ -12,79 +12,80 @@ const SessionSettings = () => {
     const title = 'Sessions'
     const description = 'Manage your active sessions'
     const router = useRouter();
-    useEffect(()=>{
-        const getSessions = async () =>{
-            const {error, data:sessions} = await authClient.listSessions()          
-            const modifiedSessions = sessions
+
+    const fetchSessions = async () => {
+        const { error, data: sessions } = await authClient.listSessions()
+        const modifiedSessions = sessions
             ?.slice()
-            .sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-            .map((session:any) => {  
+            .sort((a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+            .map((session: any) => {
                 const formattedCreatedDate = dayjs(session.createdAt).format('DD MMM YYYY, hh:mm A');
-                const formattedUpdatedDate = dayjs(session.updatedAt).format('DD MMM YYYY, hh:mm A'); 
+                const formattedUpdatedDate = dayjs(session.updatedAt).format('DD MMM YYYY, hh:mm A');
                 return {
                     ...session,
                     createdAt: formattedCreatedDate,
                     updatedAt: formattedUpdatedDate
                 }
             })
-            setSessions(modifiedSessions);
-        }
-        getSessions();
-    },[])
-
-    const handleSignoutAll = async () => {
-        const { data: currentSession} = await authClient.getSession();
-        await authClient.revokeOtherSessions();
-        setSessions((prev) => prev.filter((s) => s.token != currentSession?.token));
+        setSessions(modifiedSessions || []);
     }
 
-  return (
-    <SettingsHeader title={title} description={description}>
-        <Button variant="destructive" className="mb-4" onClick={handleSignoutAll}>Sign out from all other devices</Button>
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[200px]">Device</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Joined</TableHead>
-                    <TableHead className="text-left">Actions</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-            {sessions.map((session) => (
-                <TableRow key={session.id} className={session.isCurrent ? "bg-muted" : ""}>
-                    <TableCell>
-                    <div className="flex flex-col">
-                    <span className="font-medium">{session.userAgent?.split(' ')[0] || ''}</span>
-                        <span className="font-medium">{session.userAgent?.split(' ')[2] || 'Unknown'}</span>
-                        <span className="text-xs text-muted-foreground">{session.ipAddress ?? 'IP unknown'}</span>
-                    </div>
-                    </TableCell>
-                    <TableCell>{session.updatedAt}</TableCell>
-                    <TableCell>{session.createdAt}</TableCell>
-                    <TableCell>
-                    {!session.isCurrent && (
-                        <Button
-                        variant="destructive"
-                        size="sm"
-                        onClick={async () => {
-                            await authClient.revokeSession({ token: session.token });
-                            setSessions((prev) => prev.filter((s) => s.id !== session.id));
-                        }}
-                        >
-                        Revoke
-                        </Button>
-                    )}
-                    </TableCell>
-                </TableRow>
-                ))}
+    useEffect(() => {
+        fetchSessions();
+    }, [])
 
-            </TableBody>
-        </Table>
-        <div className='mb-20'>
-        </div>
-    </SettingsHeader>
-  )
+    const handleSignoutAll = async () => {
+        await authClient.revokeOtherSessions();
+        await fetchSessions();
+    }
+
+    return (
+        <SettingsHeader title={title} description={description}>
+            <Button variant="destructive" className="mb-4" onClick={handleSignoutAll}>Sign out from all other devices</Button>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[200px]">Device</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Joined</TableHead>
+                        <TableHead className="text-left">Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {sessions.map((session) => (
+                        <TableRow key={session.id} className={session.isCurrent ? "bg-muted" : ""}>
+                            <TableCell>
+                                <div className="flex flex-col">
+                                    <span className="font-medium">{session.userAgent?.split(' ')[0] || ''}</span>
+                                    <span className="font-medium">{session.userAgent?.split(' ')[2] || 'Unknown'}</span>
+                                    <span className="text-xs text-muted-foreground">{session.ipAddress ?? 'IP unknown'}</span>
+                                </div>
+                            </TableCell>
+                            <TableCell>{session.updatedAt}</TableCell>
+                            <TableCell>{session.createdAt}</TableCell>
+                            <TableCell>
+                                {!session.isCurrent && (
+                                    <Button
+                                        variant="destructive"
+                                        size="sm"
+                                        onClick={async () => {
+                                            await authClient.revokeSession({ token: session.token });
+                                            setSessions((prev) => prev.filter((s) => s.id !== session.id));
+                                        }}
+                                    >
+                                        Revoke
+                                    </Button>
+                                )}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+            <div className='mb-20'>
+            </div>
+        </SettingsHeader>
+    )
 }
 
 export default SessionSettings
